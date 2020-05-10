eccdbf57b6195b58e8269fc3d8ce03d7716df8fb
Park Yong Jun <pyjsh3280@kaist.ac.kr>
2020-05-10 16:06:05 +0900

    Modifying book code and add to main program

--- browncorpus.py
+++ browncorpus.py
@@ -3,44 +3,46 @@ import nltk, csv
 
 book_list = nltk.corpus.brown.categories()
 
+def Brown_hintlist(book_list):
+    
+    freq_words = list()
+    sumlist = list()
+    ans = list()
 
-freq_words = list()
-sumlist = list()
-ans = list()
+    for i in range(len(book_list)):
+        category=book_list[i]
+        book_tagged = nltk.corpus.brown.tagged_words(categories = book_list[i])
 
-for i in range(len(book_list)):
-    book_tagged = nltk.corpus.brown.tagged_words(categories = book_list[i])
+        book_word = [word for (word, tag) in book_tagged if tag in ['NN', 'NP']]
+        long_word = [word for word in book_word if word.isalpha() and len(word) > 2]
+        common_word = nltk.FreqDist(long_word).most_common()
+        clear_word = [(word, num/float(len(common_word))) for (word, num) in common_word]
+        ans.append(clear_word[:100])
+        sumlist = sumlist + clear_word[:100]
 
-    book_word = [word for (word, tag) in book_tagged if tag in ['NN', 'NP']]
-    long_word = [word for word in book_word if word.isalpha() and len(word) > 2]
-    common_word = nltk.FreqDist(long_word).most_common()
-    clear_word = [(word, num/float(len(common_word))) for (word, num) in common_word]
-    ans.append(clear_word[:100])
-    sumlist = sumlist + clear_word[:100]
+    sumlist.sort(key = lambda element : element[0])
+    newsum = list()
+    newsum.append(sumlist[0])
+    i = 0
+    for k in range(1, len(sumlist)):
+        (word, num) = sumlist[k]
+        if(not newsum[i][0] == word):
+            newsum[i] = (newsum[i][0], newsum[i][1]/len(book_list))
+            i += 1
+            if k < len(sumlist)-1: newsum.append(sumlist[k + 1])
+        else:
+            newsum[i] = (newsum[i][0], newsum[i][1] + num)
 
-sumlist.sort(key = lambda element : element[0])
-newsum = list()
-newsum.append(sumlist[0])
-i = 0
-for k in range(1, len(sumlist)):
-    (word, num) = sumlist[k]
-    if(not newsum[i][0] == word):
-        newsum[i] = (newsum[i][0], newsum[i][1]/15)
-        i += 1
-        if k < len(sumlist)-1: newsum.append(sumlist[k + 1])
-    else:
-        newsum[i] = (newsum[i][0], newsum[i][1] + num)
+    newsum[len(newsum)-1] = (newsum[len(newsum)-1][0], newsum[len(newsum)-1][1]/len(book_list))
 
-newsum[len(newsum)-1] = (newsum[len(newsum)-1][0], newsum[len(newsum)-1][1]/15)
+    newsum.sort(key = lambda element : element[1])
+    newsum.reverse()
+    remove = [word for (word, _) in newsum[:150]] # hyper parameter는 조절 가능
+    for i in range(len(ans)):
+        freq_words.append((book_list[i],[word for (word, _) in ans[i] if word not in remove]))
 
-newsum.sort(key = lambda element : element[1])
-newsum.reverse()
-remove = [word for (word, _) in newsum[:150]] # hyper parameter는 조절 가능
-for i in range(len(ans)):
-    freq_words.append([word for (word, _) in ans[i] if word not in remove])
-
-freq_words.append(remove)
-
+    freq_words.append([remove,"removing_word"])
+    return freq_words
 """
     ans = list()
     raw_sent = nltk.corpus.brown.sents(categories = book_list[i])
@@ -51,7 +53,7 @@ freq_words.append(remove)
             ans.append(t)
     common = nltk.FreqDist(ans).most_common(50)
     clear = [word for (word, num) in common if num > 2]
-"""
+
 
 
 f = open('data.csv', 'w', newline ='')
@@ -60,7 +62,6 @@ for i in range(len(book_list)+1):
     wr.writerow(freq_words[i])
 f.close()
 
-"""
 text = nltk.corpus.brown.tagged_words(fileids = ['ck01'])
 stopwords = nltk.corpus.stopwords.words('english') # to remove stopwords of English
 words = [word for (word, pos) in text if (pos == 'NP' and word.isalpha() and word.lower() not in stopwords)] # check general words
--- sample.py
+++ sample.py
@@ -1,7 +1,12 @@
 import nltk
 import requests
 from bs4 import BeautifulSoup
+import browncorpus as br
+import random
 
+book_list = nltk.corpus.brown.categories()
+wrong_list=["Nope.","Wrong answer.","Try again...", "Your answer is maybe not correct"]
+hint_list=[]
 myword=""
 corpus=""
 
@@ -12,27 +17,40 @@ think_subject function will be get the random quiz word
 """
 def think_animal():
     global myword
-    myword="jaguar"
+    animals_type=['mammals','amphibians','birds'] #'fish' 'invertebrates' 'reptiles' will be added
+    animals_dict={"mammals":['african elemphant','horse','red fox','cheetah','giraffe','koala','jaguar','arctic fox','bengal tiger','giant panda', 'polar bear'],
+                  "amphibians":['green eyed tree frog','cane toad','spotted salamander'],
+                  "fish":[],
+                  "birds": ['bald eagle','house sparrow','america crow','rock pigeon'],
+                  "invertebrates":[],
+                  "reptiles":[]}
+    rand_type=animals_type[random.randint(0,len(animals_type)-1)]
+    rand_list=animals_dict[rand_type]
+    myword=rand_list[random.randint(0,len(rand_list)-1)]
+    return (rand_type, myword)
 def think_greats():
     global myword
     myword="Einstein"
-def think_hi():
+def think_book():
     global myword
-    myword="hi"
+    myword=book_list[random.randint(0,len(book_list)-1)]
+    return myword
 
 """
 subject_corpus function gets the imformation of quiz word from website.
 The website will be different to have appropriate informations.
 ex) I  select the national geographic site to get the features of specific animals.
 """
-def animal_corpus(word):
+def animal_corpus(animal_type, word):
     corpus=""
-    web=requests.get('https://www.nationalgeographic.com/animals/mammals/'+word[0]+'/'+word+'/')
+    web=requests.get('https://www.nationalgeographic.com/animals/'+animal_type+'/'+word[0]+'/'+"-".join(word.split())+'/')
     soup=BeautifulSoup(web.content, 'html.parser')
     feature_list= soup.findAll('div',class_='smartbody')
     for sent in feature_list:
         untag=sent.get_text()
         corpus += untag + " "
+        corpus = corpus.replace('\n','')
+        corpus = corpus.replace("'","")
     return corpus
 
 """
@@ -40,35 +58,79 @@ def animal_corpus(word):
 Select the subject with user interation &
 select quiz word + get information from web (make corpus to answer the user questions)
 """
-def choose_subect():    
+def choose_subject():    
     while (1):
         print("If you want to exit, give me the number 0")
         print("Choose the subject.")
-        sub=input("1. animals "+"2.greats "+"3. hi "+"\n")
+        sub=input("1. animals "+"2.greats "+"3. book categories "+"\n")
         if(sub=='0'):
-            break
+            return 0
         if(sub=='1'):
             global corpus
-            think_animal()
-            corpus=animal_corpus(myword)
+            (animal_type, animal)=think_animal()
+            corpus=animal_corpus(animal_type,animal)
+            return 1
         elif(sub=='2'):
             think_greats()
             #corpus=greates_corpus(myword)
-            break
+            return 2
         elif(sub=="3"):
-            think_hi()
-            #corpus=
-            break
+            global hintlist
+            print("I'm working...")
+            rand_category=think_book()
+            category_word_list=br.Brown_hintlist(book_list)
+            for (category, word_list) in category_word_list:
+                if (rand_category==category):
+                    hintlist=word_list
+                    break
+            return 3
         else:
             print("There is no option about that. Choose number.\n")
 
 def get_questions():
+    for i in range(20):
+        print("This is "+str(i+1)+"th question")
+        quest=input("What's your question? Please use question mark \'?\' in the end. \n You can give me the answer also.\n")
+        if (quest==myword):
+            print("You correct! Congratulation. The answer was "+myword+"!")
+            return
+        if (quest[-1]!='?'):
+            print(wrong_list[random.randint(0,len(wrong_list)-1)])
+            continue
+        print("I'm thinking about your question...")
+        sentence=""
+        #sentence=dealing_question(quest)
+        print("My answer: "+sentence+"\n")
+    print("20 questions over... I win zz. The answer was "+myword+"!")
+
+def give_hint():
+    print("I will give hints to you rather than I get the questions from you.")
+    for i in range(20):
+        print("This is "+str(i+1)+"th hints")
+        quest=input("Give me the answer or give me \'?\' to get hints.\n")
+        if (quest==myword):
+            print("You correct! Congratulation.")
+            return
+        elif (quest[-1]=='?'):
+            global hintlist
+            print("My hint word is : "+ hintlist[0])
+            hintlist= hintlist[1:]
+            continue
+        print(wrong_list[random.randint(0,len(wrong_list)-1)])
+    print("20 questions over... I win zz. The answer was "+myword+"!")
+    
+def dealing_question(quest):
+    pass
 
 #(not completed)
-#main function part
-            
-print("Hello, this is 20 questions program.\n"+"I will think the word about subjects  you chose.\n")
-choose_subject()
-#get_questions (get user question + check the answer)
-#answer_questions (with some tricks)
-#
+#main function part            
+print("Hello, this is 20 questions program.\n"+"I will think the word about subject  you chose.\n")
+exit_code=choose_subject()
+if (exit_code == 3):
+    give_hint()
+elif (exit_code!=0):
+    get_questions()
+    #dealing_question
+print("Good bye.\n")
+
+
